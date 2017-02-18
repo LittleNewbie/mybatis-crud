a0bd0f25720747b3965d1e2f2af79cad21d3cf60
LittleNewbie <153955424@qq.com>
2017-02-18 19:03:49 +0800

    version 2.0

---
+++ src/main/java/com/svili/portal/crud/common/NumberUtil.java
@@ -0,0 +1,46 @@
+package com.svili.portal.crud.common;
+
+import java.math.BigDecimal;
+
+/**
+ * Number格式化工具类
+ * 
+ * @author svili
+ * @date 2017年2月18日
+ *
+ */
+public class NumberUtil {
+
+	public static byte toByte(Number number) {
+		return number.byteValue();
+	}
+
+	public static double toDouble(Number number) {
+		return number.doubleValue();
+	}
+
+	public static float toFloat(Number number) {
+		return number.floatValue();
+	}
+
+	public static int toInt(Number number) {
+		return number.intValue();
+	}
+
+	public static long toLong(Number number) {
+		return number.longValue();
+	}
+
+	public static short toShort(Number number) {
+		return number.shortValue();
+	}
+
+	public static BigDecimal toBigDecimal(Number number) {
+		if (number instanceof BigDecimal) {
+			return (BigDecimal) number;
+		} else {
+			return new BigDecimal(number.doubleValue());
+		}
+	}
+
+}
---
+++ src/main/java/com/svili/portal/crud/common/StringUtil.java
@@ -0,0 +1,75 @@
+package com.svili.portal.crud.common;
+
+import java.util.List;
+
+/**
+ * 字符串工具类
+ * 
+ * @author svili
+ * @date 2016年11月8日
+ *
+ */
+public class StringUtil {
+
+	/**
+	 * 将驼峰标识转换为下划线
+	 * 
+	 * @param text
+	 * @return camel
+	 */
+	public static String camelToUnderline(String text) {
+		if (text == null || "".equals(text.trim())) {
+			return "";
+		}
+		StringBuffer result = new StringBuffer(text.length() + 1);
+		result.append(text.substring(0, 1));
+		for (int i = 1; i < text.length(); i++) {
+			if (!Character.isLowerCase(text.charAt(i))) {
+				result.append('_');
+			}
+			result.append(text.substring(i, i + 1));
+		}
+		return result.toString().toLowerCase();
+	}
+
+	/**
+	 * 将下划线标识转换为驼峰
+	 * 
+	 * @param text
+	 * @return underline
+	 */
+	public static String underlineToCamel(String text) {
+		if (text == null || "".equals(text.trim())) {
+			return "";
+		}
+		int length = text.length();
+		StringBuffer result = new StringBuffer();
+		for (int i = 0; i < length; i++) {
+			char c = text.charAt(i);
+			if (c == '_') {
+				if (++i < length) {
+					result.append(Character.toUpperCase(text.charAt(i)));
+				}
+			} else {
+				result.append(c);
+			}
+		}
+		return result.toString();
+	}
+
+	/**
+	 * 将String list字符串集合用逗号","分割
+	 * 
+	 * @param list
+	 *            集合
+	 * @return string
+	 */
+	public static String splitListByComma(List<String> list) {
+		StringBuffer result = new StringBuffer();
+		for (String str : list) {
+			result.append(",").append(str);
+		}
+		return result.substring(1, result.length());
+	}
+
+}
